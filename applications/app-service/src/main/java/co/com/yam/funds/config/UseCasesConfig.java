99931111263038613cd85aa3006a36a6b1b9a707
Piter Velasquez <pealveco@gmail.com>
2026-08-11 12:05:27 -0500

    Add initial project structure with Spring Boot configuration and Gradle setup

---
+++ applications/app-service/src/main/java/co/com/yam/funds/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.yam.funds.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.yam.funds.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
